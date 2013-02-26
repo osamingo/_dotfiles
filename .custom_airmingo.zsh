b481b4f14ed3e682cb6993028e8b1599b7a78d2e
osamingo <osamingo@gmail.com>
2013-02-27 02:41:07 +0900

    edit custom zsh

--- .custom_airmingo.zsh
+++ .custom_airmingo.zsh
@@ -1,17 +1,23 @@
 # alias
+# http://mba-hack.blogspot.jp/2012/07/tmuxvi.html
 alias tm='/usr/local/bin/tmuxx'
 
 # zsh-completions 
+# brew install zsh-completions
 autoload -U compinit; compinit
 fpath=(/usr/local/share/zsh-completions $fpath)
 
 # rubyenv
+# http://pplog.org/?p=1845
 eval "$(rbenv init - zsh)"
 
 # mosh
+# brew install mobile-shell
+# moshコマンドでも、sshコマンドのcompletions有効化
 compdef mosh=ssh
 
 # z
+# brew install z
 . `brew --prefix`/etc/profile.d/z.sh
 function precmd () {
 	z --add "$(pwd -P)"
